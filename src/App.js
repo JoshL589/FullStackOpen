359ddc768eb68e0dd9df0ea9122ecd31d59c0ffd
Joshua Li <joshuazli2020@gmail.com>
2022-11-30 16:54:29 +1000

    more edits

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ const History = (props) => {
   if (props.allClicks.length === 0) {
     return <div>the app is used by pressing the buttons</div>;
   }
-  return <div>button press History: {props.allClicks.join(" ")}</div>;
+
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   const [left, setLeft] = useState(0);
@@ -25,8 +30,8 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {right}
       <History allClicks={allClicks} />
     </div>
